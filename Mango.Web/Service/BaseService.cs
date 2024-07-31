7393c3be9a8b688f8e40a27dbd2d7e6556e5eefe
gbao21 <tranbao2122@gmail.com>
2024-07-31 22:39:35 +0700

    bao

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,49 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.ISErvice;
+using Newtonsoft.Json;
+using System.Text;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDto?> IBaseService.SendAsync(RequestDto requestDTO)
+        {
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Content-Type", "application/json");
+            //token
+
+            message.RequestUri = new Uri(requestDTO.Url);
+            if(requestDTO.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDTO.Data), Encoding.UTF8,"application/json");
+            }
+
+            HttpResponseMessage? apiResponse = null;
+            switch (requestDTO.ApiType)
+            {
+                case ApiType.POST:
+                    message.Method = HttpMethod.Get;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Get;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Get;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+        }
+    }
+} 
---
+++ Mango.Web/Service/ISErvice/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.ISErvice
+{
+    public interface IBaseService
+    {
+      Task<ResponseDto?>  SendAsync(RequestDto requestDTO);
+    }
+}
